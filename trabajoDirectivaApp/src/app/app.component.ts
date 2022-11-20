72e8374a062ba5861c2bf642cbb880e9a25da4fe
erikahernande <erika.hernandezmay@cbtis72.edu.mx>
2022-11-20 15:09:03 -0500

    "Se sube ngFor"

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas: object[];
+
+  constructor(){
+
+    this.entradas=[
+      
+      {titulo: "Python cada dia mas presente"},
+      {titulo: "Java presente desde hace más de 20 años"},
+      {titulo: "JS cada vez más funcional"},
+      {titulo: "Kotlin potencia para tus apps"},
+      {titulo: "¿Dondé quedo pascal?"},
+    ]
+  }
+
+  registrarUsuario(){
+    this.registrado=true;
+    this.mensaje= "Usuario registrado con exito"
+  }
 }
